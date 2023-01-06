dd01f7b578054fec5fa1a9c71e2a502b900c4836
Yauheni Paulavets <yauheni.paulavets@coople.com>
2023-01-06 14:23:27 +0100

    [*] Changed schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,11 +40,14 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
+    durationInSeconds: ({ length }) => length,
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
+  Module: {
+    durationInSeconds: ({ length }) => length,
+  }
 };
 
 module.exports = resolvers;
